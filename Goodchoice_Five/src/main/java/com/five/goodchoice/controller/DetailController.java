671ed08b9d7f7b3815b181b2a10f6d55b66bbf4b
Andy <fusrqdoem8192@gmail.com>
2023-05-22 18:01:07 +0900

    jy's

---
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/controller/DetailController.java
@@ -0,0 +1,5 @@
+package com.five.goodchoice.controller;
+
+public class DetailController {
+
+}
--- Goodchoice_Five/src/main/java/com/five/goodchoice/controller/IndexController.java
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/controller/IndexController.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 	
-	@	RequestMapping(value="/") //	http://localhost:9090/goodchoice/
+	@	RequestMapping(value="/")
 	public ModelAndView home ( ModelAndView mav) {
 		
 		mav.setViewName("redirect:/index.gc");		
 		return mav ;
 	}
 		
-	@	RequestMapping(value="/index.gc")//   http://localhost:9090/goodchoice/index.gc  로 검색해보세요
+	@	RequestMapping(value="/index.gc")//   http://localhost:9090/index/      로 검색해보세요
 	public ModelAndView index ( ModelAndView mav) {
 		mav.setViewName("index");		
 		return mav ;
@@ -23,30 +23,8 @@ public class IndexController {
 	@RequestMapping(value="/test/tiles_test_1.gc") // tiles test 입니다.
 	public String tiles_test_1() {
 		
-		return "tile_test.tiles1";
+		return "tile_test.tiles1" ;
 	}
-	
-	@RequestMapping(value="/test/tiles_test_2.gc") 
-	public String tiles_test_2() {
-		return "product/content.tiles2";
-	}
-	
-	@RequestMapping(value="/test/tiles_test_3.gc") // tiles2 
-	public String tiles_test_3() {
-		return "test/test.tiles2";
-	}
-	
-	// 메인용 
-	@RequestMapping(value="/main/home.gc") // tiles test 입니다.
-	public String main() {
-		
-		return "main/home.tiles1" ;
-		// /WEB-INF/views/tiles1/{1}/{2}.jsp
-	}
-	
-
-	
-	
 
 	
 }
