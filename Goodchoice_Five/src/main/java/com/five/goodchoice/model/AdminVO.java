c30df11a58cc5724c13664b618aaa7d1d85f0f4c
user <user@LAPTOP-IN1OIPHK>
2023-05-25 15:14:43 +0900

    관리자

---
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/model/AdminVO.java
@@ -0,0 +1,10 @@
+package com.five.goodchoice.model;
+
+public class AdminVO {
+	 private int admin_no;           /* 관리자번호 */
+	 private String admin_email;     /* 관리자이메일 */
+	 private String admin_pwd;       /* 관리자비밀번호 */
+	 private String admin_name;     /* 관리자이름 */
+	 private String admin_reg_date; /* 관리자등록일 */
+
+}
