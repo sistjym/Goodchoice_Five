b402481d9d35c8e2131e4bafea5bf879e11b1298
user <user@LAPTOP-IN1OIPHK>
2023-05-25 16:41:17 +0900

    관리자

--- Goodchoice_Five/src/main/java/com/five/goodchoice/model/service/AdminService.java
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/model/service/AdminService.java
@@ -1,8 +1,23 @@
 package com.five.goodchoice.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.five.goodchoice.member.model.InterAdminDAO;
+
 
 @Service
 public class AdminService implements InterAdminService {
+
+	@Autowired
+	 private InterAdminDAO dao;
+	//권한을 승인하길 기다리는 모든 호스트를 보여주기 
+	@Override
+	public List<String> HostList() {
+		
+		return null;
+	}
 
 }
--- Goodchoice_Five/src/main/java/com/five/goodchoice/model/service/InterAdminService.java
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/model/service/InterAdminService.java
@@ -1,5 +1,10 @@
 package com.five.goodchoice.model.service;
 
+import java.util.List;
+
 public interface InterAdminService {
+	
+	//권한을 승인하길 기다리는 모든 호스트를 보여주기 
+	List<String> HostList();
 
 }
