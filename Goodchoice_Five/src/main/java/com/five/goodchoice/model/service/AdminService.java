70ed1309f85a0b43256653571e973e3818af9828
user <user@LAPTOP-IN1OIPHK>
2023-05-31 12:33:35 +0900

    모든숙소보기 기능 추가

--- Goodchoice_Five/src/main/java/com/five/goodchoice/model/service/AdminService.java
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/model/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.five.goodchoice.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,34 @@ public class AdminService implements InterAdminService {
 
 	@Autowired
 	 private InterAdminDAO dao;
+	
+	
 	//권한을 승인하길 기다리는 모든 호스트를 보여주기 
 	@Override
 	public List<String> HostList() {
 		
 		return null;
+	}
+	
+	// 모든 숙소 보여주기 
+	@Override
+	public List<Map<String, String>> getAcommList(Map<String, Object> paraMap) {
+		List<Map<String, String>> acommList = dao.getAcommList(paraMap);
+		return acommList;
+	}
+
+	// 카테고리 리스트 가져오기 
+	@Override
+	public List<String> categoryNameList() {
+		List<String> categoryList = dao.categoryNameList();
+		return categoryList;
+	}
+
+	// 분류 리스트 가져오기 
+	@Override
+	public List<String> specNameList() {
+		List<String> specList = dao.specNameList();
+		return specList;
 	}
 
 }
