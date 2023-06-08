e7d01079923da272a2d2e490d66898787ab87cde
user <user@LAPTOP-IN1OIPHK>
2023-06-08 15:05:07 +0900

    예약 패키지 생성

---
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/reservation/model/InterReservationDAO.java
@@ -0,0 +1,5 @@
+package com.five.goodchoice.reservation.model;
+
+public interface InterReservationDAO {
+
+}
---
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/reservation/model/ReservationDAO.java
@@ -0,0 +1,5 @@
+package com.five.goodchoice.reservation.model;
+
+public class ReservationDAO implements InterReservationDAO {
+
+}
