7dc7f17ec3d1e5241bf44743c9538a8426e6f673
Andy <fusrqdoem8192@gmail.com>
2023-06-15 15:40:45 +0900

    jyjyjyjyjy

--- Goodchoice_Five/src/main/java/com/five/goodchoice/reservation/model/InterReservationDAO.java
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/reservation/model/InterReservationDAO.java
@@ -4,10 +4,10 @@ import java.util.Map;
 
 public interface InterReservationDAO {
 
-	// 결제가 성공적으로 이루어지면 예약테이블에 정보 넣기 
-	int insertReservationInfo(Map<String, String> paraMap);
+   // 결제가 성공적으로 이루어지면 예약테이블에 정보 넣기 
+   int insertReservationInfo(Map<String, String> paraMap);
 
-	// 사용한 초인트 뻬고 적립될 포인트 더하기
-	int updatePoint(Map<String, String> paraMap);
+   // 사용한 초인트 뻬고 적립될 포인트 더하기
+   int updatePoint(Map<String, String> paraMap);
 
 }
--- Goodchoice_Five/src/main/java/com/five/goodchoice/reservation/model/ReservationDAO.java
+++ Goodchoice_Five/src/main/java/com/five/goodchoice/reservation/model/ReservationDAO.java
@@ -10,25 +10,25 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ReservationDAO implements InterReservationDAO {
 
-	
-	@Resource
-	private SqlSessionTemplate sqlsession; // 로컬 DB mymvc_user에 연결 
-	// Type 에 따라 Spring 컨테이너가 알아서 root-context.xml 에 생성된 org.mybatis.spring.SqlSessionTemplate 의  sqlsession bean 을  sqlsession 에 주입시켜준다. 
+   
+   @Resource
+   private SqlSessionTemplate sqlsession; // 로컬 DB mymvc_user에 연결 
+   // Type 에 따라 Spring 컨테이너가 알아서 root-context.xml 에 생성된 org.mybatis.spring.SqlSessionTemplate 의  sqlsession bean 을  sqlsession 에 주입시켜준다. 
     // 그러므로 sqlsession 는 null 이 아니다.
-	
-	
-	// 결제가 성공적으로 이루어지면 예약테이블에 정보 넣기 
-	@Override
-	public int insertReservationInfo(Map<String, String> paraMap) {
-		int n = sqlsession.insert("reservation.insertReservationInfo", paraMap);
-		return n;
-	}
+   
+   
+   // 결제가 성공적으로 이루어지면 예약테이블에 정보 넣기 
+   @Override
+   public int insertReservationInfo(Map<String, String> paraMap) {
+      int n = sqlsession.insert("reservation.insertReservationInfo", paraMap);
+      return n;
+   }
 
-	// 사용한 초인트 뻬고 적립될 포인트 더하기
-	@Override
-	public int updatePoint(Map<String, String> paraMap) {
-		int n = sqlsession.insert("reservation.updatePoint", paraMap);
-		return n;
-	}
+   // 사용한 초인트 뻬고 적립될 포인트 더하기
+   @Override
+   public int updatePoint(Map<String, String> paraMap) {
+      int n = sqlsession.insert("reservation.updatePoint", paraMap);
+      return n;
+   }
 
 }
